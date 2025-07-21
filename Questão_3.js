12c472cbb4a61586fc5c217983f801752c9c06e7
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-07-21 00:35:06 -0300

    Questão_3.js

--- Questão_3.js
+++ Questão_3.js
@@ -1,3 +1,5 @@
+// Q3. Imprimir quantidade de elementos de uma lista
+
 const lista = [3, 5, -1, 4, 0]
 const quantidadeElementos = (lista) => lista.length
 const quantidadeElementosLista = quantidadeElementos(lista)
